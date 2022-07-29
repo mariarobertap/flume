370f0bcbb3dbe82508a1d163d5ae272e02cc0d9f
Maria Roberta <mariarobertaprado@outlook.com>
2022-07-28 22:55:06 -0300

    [New] - adding project folders - wpp signin page

---
+++ App.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { LogBox, Text, View } from 'react-native';
+import { useAssets } from "expo-asset"
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+import {NavigationContainer} from "@react-navigation/native"
+import {createStackNavigator} from "@react-navigation/stack"
+import SignIn from "./screens/SignIn"
+import ContextWrapper from './context/ContextWrapper';
+
+LogBox.ignoreLogs([
+  "Setting a timer",
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release.",
+]);
+const Stack = createStackNavigator();
+
+function App() {
+  const [currUser, setCurrUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect( () => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if(user){
+        setCurrUser(user);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if(loading){
+    return <Text>Loading</Text>
+  }
+
+
+  return (
+    <NavigationContainer>
+    {!currUser ?(
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='signIn' component={SignIn}/>
+      </Stack.Navigator>
+    ):(
+      <Text>Hi user</Text>
+    )
+    }
+      <Text>{JSON.stringify(currUser)}</Text>
+    </NavigationContainer>
+  );
+}
+
+
+
+function Main(){
+  const [assets] = useAssets(
+    require("./assets/icon-square.png"),
+    require("./assets/chatbg.png"),
+    require("./assets/user-icon.png"),
+    require("./assets/welcome-img.png")
+  );
+
+  if (!assets){
+    return <Text>Loading..</Text>
+  }
+
+  return ( <ContextWrapper> 
+              <App/>
+           </ContextWrapper>);
+}
+
+export default Main
---
+++ context/Context.js
@@ -0,0 +1,13 @@
+
+
+import React, { useState } from "react";
+import { theme } from "../utils";
+
+
+
+
+const GlobalContext = React.createContext({
+    theme
+});
+
+export default GlobalContext;
